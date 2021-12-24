83e09350ab59af096876cff9cb6c35372bc05cf7
prem2219 <81369141+prem2219@users.noreply.github.com>
2021-12-25 00:38:22 +0530

    1

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  security_groups= [var.security_group]
+  security_groups= [var.security_group.name]
   tags= {
     Name = var.tag_name
   }
