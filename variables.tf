422fb097cd99be406e170b27c3f3a1e4b96e4346
prem2219 <81369141+prem2219@users.noreply.github.com>
2021-12-24 23:56:05 +0530

    updated latest

--- variables.tf
+++ variables.tf
@@ -25,12 +25,12 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  default     = "my-jenkins-security-group1"
 }
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  default     = "my-ec2-instance1"
 }
 
 variable "ami_id" {
